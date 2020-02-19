88294fb2af0ce8d40075f9bf16133c33efb980c3
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:30:02 +0530

    Initial commit

---
+++ callbyreference/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callbyreference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            a = 10;
+            b = 20;
+            ShowData(ref a,ref  b);
+            Console.ReadKey();
+        }
+        static void ShowData(ref int m, ref int n)
+        {
+         
+            double sum = m + n;
+            Console.WriteLine("sum={0}", sum);
+            m = m + 2;
+            n = n + 2;
+        }
+    }
+}
